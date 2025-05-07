31ef117c4fed1b37b1ce1e7b66215f2178ff542a
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-07 20:19:10 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -14,10 +14,13 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "SPIDEY777")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5518489725').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002423451263'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001959922658") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "true")
+    FORCE_SUB_CHANNEL = list(map(int, getenv("FORCE_SUB_CHANNEL", "-1001959922658,-1002470391435,-1002433552221").replace(" ", "").split(",")))
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
-    
+
+#starting imgs
+    START_IMG = (environ.get('START_IMG', 'https://graph.org/file/2518d4eb8c88f8f669f4c.jpg https://graph.org/file/d6d9d9b8d2dc779c49572.jpg https://graph.org/file/4b04eaad1e75e13e6dc08.jpg https://graph.org/file/05066f124a4ac500f8d91.jpg https://graph.org/file/2c64ed483c8fcf2bab7dd.jpg https://i.ibb.co/CPxdkHR/IMG-20240818-192201-633.jpg')).split()
+        
 #Dont Remove My Credit @Silicon_Bot_Update 
 #This Repo Is By @Silicon_Official 
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
